cc5bfba31f4dc774ff5f37dd76dfe28efc7a8981
elena214 <elebe2104@gmail.com>
2020-11-16 11:26:02 +0100

    Codice ASCII

---
+++ CodiceASCII/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CodiceASCII
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
