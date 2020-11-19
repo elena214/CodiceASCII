b160ab2c8bdb7617697c1c64aa5b4a982fe2ebc6
elena214 <elebe2104@gmail.com>
2020-11-19 12:22:19 +0100

    Codice ASCII mgliorato

--- CodiceASCII/Program.cs
+++ CodiceASCII/Program.cs
@@ -6,7 +6,16 @@ namespace CodiceASCII
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Inserisci un valore numerico o alfanumerico da convertire in ASCII:");
+            char carattere = Convert.ToChar(Console.ReadLine());
+            int number = carattere;
+            // aggiungere 4 al numero decimale corrispondente della lettera inserita
+            // number = number + 4;
+            Console.WriteLine($"Il carattere digitato corrisponde al numero: {number.ToString()}");
+            Console.WriteLine(number.ToString());
+            String binario = Convert.ToString(number, 2).PadLeft(8, '0');
+            Console.WriteLine($"Il carattere in codice binario corrisponde alla sequenza di bit {binario}");
+            Console.ReadLine();
         }
     }
 }
